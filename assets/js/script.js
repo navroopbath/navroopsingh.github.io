0769d5cec6348d4a0f02c4a50efb2c7b2dbc8998
Navroop Singh <roopsingh93@gmail.com>
2015-04-27 10:14:29 -0700

    navbar is now sticky

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,20 @@ $(document).ready(function(){
 	
 	$('#contact').click(function() {
 		$('#modal-container').fadeIn();
+        $('nav').addClass('hide-nav');
 	});
 
-    $('#submit').click(function() {
+    $('#close').click(function() {
 		$('#modal-container').hide();
+        $('nav').removeClass('hide-nav');
 	});
 	
 	$('#modal-overlay').click(function () {
 		$('#modal-container').hide();
+        $('nav').removeClass('hide-nav');
 	});
+    
+    $('li').hover(function() {
+        $(this).addClass('.hover');
+    });
 });
